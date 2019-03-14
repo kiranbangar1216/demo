17eb41f620621158f18df93099989611a25dd7e3
kiranbangar1216 <kiranbangar1216@gmail.com>
2019-03-14 18:09:59 +0530

    Learning vue components,methods.

---
+++ src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="user">
+<p>Name:{{detail.name}}</p>
+<p>About:{{detail.about}}</p>
+<p>Dob:{{detail.dob | formatDob}}</p>
+</div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'User',
+  props: ['detail'],
+  filters: {
+    formatDob(value) {
+      if (value) {
+        return moment(value).format('Do MMMM YYYY,dddd');
+      }
+      return 'No date found';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user {
+  border: 5px;
+  padding: 10px;
+}
+</style>
---
+++ src/views/Contactus.vue
@@ -0,0 +1,63 @@
+<template>
+<div><h3>{{header}}</h3>
+<p>{{message}}</p>
+<div>
+<input  type="text" v-model="message">
+    </div>
+    <p>
+<button @click="myFunction()">Click Me!</button>
+</p>
+<User v-for="person in team" v-bind:detail="person">
+</User>
+</div>
+</template>
+
+<script>
+import User from "@/components/User.vue";
+import moment from "moment";
+
+export default {
+  name: "contactus",
+  components: {
+    User
+  },
+  created() {
+    this.initialcall();
+  },
+  data() {
+    return {
+      message: "Hello Vue!",
+      header: "Love apis!",
+      team: [
+        {
+          name: "roshan",
+          about: "uideveloper",
+          dob: moment("20-10-1995", "DD-MM-YYYY")
+        },
+        {
+          name: "vicky",
+          about: "meanstack",
+          dob: moment("27-1-1995", "DD-MM-YYYY")
+        },
+
+        {
+          name: "nayan",
+          about: "ui/ux developer",
+          dob: moment("29-10-1991", "DD-MM-YYYY")
+        }
+      ]
+    };
+  },
+  methods: {
+    // initialcall() {
+    //   this.header = "vue";
+    // },
+    myFunction() {
+      this.message = "Bye Vue!!";
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
